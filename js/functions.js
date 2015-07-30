c95ff3ae79e580296872446c3b6da192d9255eed
Brandon Baum <railsdev14@gmail.com>
2015-07-30 16:00:50 -0400

    first commit

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+document.getElementById("scope").addEventListener("click", function (e) {
+    var target = e.target;
+
+    target.classList.toggle("iconize");
+    target.classList.toggle("iconize2");
+}, false);
