b54d7fdfb1ef5f8f54ec178b691b3f82d31399de
Brandon Baum <railsdev14@gmail.com>
2015-07-31 00:11:50 -0400

    Added scss and js

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,29 @@
-document.getElementById("scope").addEventListener("click", function (e) {
-    var target = e.target;
+// showing
+document.getElementById('scope-info').style.display = '';
+// hiding
+document.getElementById('scope-info').style.display = 'none';
+// Scope of Work section
+document.getElementById("toggle").onclick = function(){
+	var s = document.getElementById("scope-info");
+	if(s.style.display != 'none') {
+		s.style.display = 'none';
+	} else {
+		s.style.display = '';
+	}
+}
 
-    target.classList.toggle("iconize");
-    target.classList.toggle("iconize2");
-}, false);
+// showing
+document.getElementById('faq-info').style.display = '';
+// hiding
+document.getElementById('faq-info').style.display = 'none';
+// FAQ section
+document.getElementById("toggle2").onclick = function(){
+	var f = document.getElementById("faq-info");
+	if(f.style.display != 'none') {
+		f.style.display = 'none';
+	} else {
+		f.style.display = '';
+	}
+}
+
+
